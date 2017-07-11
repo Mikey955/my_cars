64482cb64bc80e009f2ddba2b2380811b5dc788b
Mikey955 <michaelmenezes_995@hotmail.com>
2017-07-11 14:14:52 +0530

    2 files deleted

--- hyundai.py
+++
@@ -1,6 +0,0 @@
-def disp():
-    print "Hyundai Motor India Ltd is a wholly owned subsidiary of the Hyundai Motor Company in India."
-
-disp()
-disp()
-disp() 
--- toyota.py
+++
@@ -1,6 +0,0 @@
-def disp():
-    print "Toyota Motor Corporation is a Japanese multinational automotive manufacturer headquartered in Toyota"
-
-disp()
-disp()
-disp()
