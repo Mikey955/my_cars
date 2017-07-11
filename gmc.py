e458505e93a252c9b194de60d714076b64a78f4d
Mikey955 <michaelmenezes_995@hotmail.com>
2017-07-11 14:18:57 +0530

    gmc modified

--- gmc.py
+++ gmc.py
@@ -2,5 +2,3 @@ def disp():
     print "GMC (General Motors Truck Company), formally the GMC Division of General Motors LLC, is a division of the American automobile manufacturer General Motors."
 
 disp()
-disp()
-disp()
